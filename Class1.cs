5726d59344286839f35e6f5be65fbcdf1714a77d
mounika <mounaadagatla@gmail.com>
2021-09-17 14:50:58 +0530

    [EmpStatus ]added: is employee present or not

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class EmpStatus
+{
+	public EmpStatus()
+	{
+		const int IS_PRESENT = 1;
+		public void Attendance()
+		{
+			Random random = new Random()
+				int employeecheck = random.Next(0, 2);
+			if (employeecheck == IS_PRESENT)
+				Console.WriteLine("Present");
+			else
+				Console.WriteLine("Absent");
+		}
+		EmpStatus e = new EmpStatus();
+		e.Attendance();
+		Console.ReadLine();
+	}
+}
